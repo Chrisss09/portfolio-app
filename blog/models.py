0131c6a8d9999e7b1355aeeee5114a7aa41c8ddf
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-08-26 18:43:25 +0100

    Adding to my blog page with unique ID's

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,12 @@ class Blog(models.Model):
     created_on = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def created_on_pretty(self):
+        return self.created_on.strftime('%b %e %Y')
